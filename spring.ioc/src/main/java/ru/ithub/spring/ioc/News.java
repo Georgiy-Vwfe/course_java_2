6d3bc17798f57004afaf3dbe60d3e0bd6445dd02
Georgiy-Vwfe <postnikov.george@yandex.ru>
2019-03-15 10:27:59 +0300

    ioc

--- spring.ioc/src/main/java/ru/ithub/spring/ioc/News.java
+++ spring.ioc/src/main/java/ru/ithub/spring/ioc/News.java
@@ -1,9 +1,12 @@
 package ru.ithub.spring.ioc;
 
 public class News {
-    private String content;
+    private String content = "";
     private String title;
 
+    public News(String title) {
+        this.title = title;
+    }
 
     public String getContent() {
         return content;
@@ -11,5 +14,18 @@ public class News {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public void addLine(String text) {
+        if(text != null)
+            this.content += text + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return "News{" +
+                "content='" + content + '\'' +
+                ", title='" + title + '\'' +
+                '}';
     }
 }
