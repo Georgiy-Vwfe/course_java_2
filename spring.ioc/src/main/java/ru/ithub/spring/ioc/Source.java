2b83f8ca827deee412a7a15b5851cf6e270340f3
Georgiy-Vwfe <31819579+Georgiy-Vwfe@users.noreply.github.com>
2019-03-16 11:54:48 +0300

    InternetSource

--- spring.ioc/src/main/java/ru/ithub/spring/ioc/Source.java
+++ spring.ioc/src/main/java/ru/ithub/spring/ioc/Source.java
@@ -1,8 +1,10 @@
 package ru.ithub.spring.ioc;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 
 public interface Source {
 
-    InputStream getStream();
+    InputStream getStream() throws IOException;
 }
